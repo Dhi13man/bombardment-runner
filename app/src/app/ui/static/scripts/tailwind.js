2dffc32c06f6306c29cb9b6279219e42032f0039
dhi13man <furyx.ds@gmail.com>
2025-05-05 00:17:09 +0530

    feat(ui): Refactor Tailwind CSS configuration and improve validation styles

---
+++ app/src/app/ui/static/scripts/tailwind.js
@@ -0,0 +1,15 @@
+// Tailwind CSS configuration
+tailwind.config = { 
+  theme: { 
+    extend: {
+      colors: {
+        'primary': '#F97316',
+        'primary-light': '#FFAC6A',
+        'primary-dark': '#C75A1D',
+        'secondary': '#FDAE42',
+        'secondary-light': '#FFE3B8',
+        'accent': '#FF6C37',
+      }
+    } 
+  } 
+};
